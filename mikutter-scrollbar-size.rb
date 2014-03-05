a38232ae9251c4af2a1e1ef39bad8446fbf6f776
moguno <moguno@0kn.sakura.ne.jp>
2014-03-05 23:03:11 +0900

    はじめてのコミット。ワクワクが止まらない。

---
+++ mikutter-scrollbar-size.rb
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+Plugin.create :scrollbar_size do
+  UserConfig[:scrollbar_size] ||= 24
+
+  settings "スクロールバーのアイコンサイズ" do
+      adjustment("スクロールバーの幅（再起動必要）", :scrollbar_size, 1, 100)
+  end
+
+  Gtk::RC.parse_string <<-EOS
+    style "scrollbar"
+    {
+      GtkRange::slider_width = #{UserConfig[:scrollbar_size]}
+      GtkRange::stepper_size = #{UserConfig[:scrollbar_size]}
+    }
+
+    class "GtkRange" style "scrollbar"
+  EOS
+
+end
---
+++ spec
@@ -0,0 +1,11 @@
+---
+slug: :mikutter-scrollbar-size
+depends:
+  mikutter: 0.2.2.1476
+  plugin:
+  - settings
+  - gtk
+version: '1.0'
+author: moguno
+name: "スクロールバーのサイズ変更"
+description: "スクロールバーのサイズを変更します。タッチインターフお供に。"
